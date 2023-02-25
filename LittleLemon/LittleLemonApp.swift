724d2b7feb3bf9f8aa49a289a987178baf127e97
JohnX4321 <johnx4321gaming@gmail.com>
2023-02-25 11:37:54 +0530

    Initial Commit

---
+++ LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Samarth MS on 25/02/23.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
